17be56f7f38793a36a914bbe6e4a63b19faaecd3
Satyam Gupta <97394641+satyamg1620@users.noreply.github.com>
2023-04-21 21:29:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# try:
 import os
 import streamlit as st
 import matplotlib.pyplot as plt 
@@ -8,15 +7,11 @@ from PIL import Image
 import imageio as imageio
 from io import BytesIO, StringIO
 import requests
-# 	print("All Modules Loaded")
-# except Exception as e:
-# 	print(f"Some Modules are missing : {e}")
+
 
 st.set_page_config(page_title="PCA Image Reconstruction", page_icon=":tada:", layout="wide")
 with st.container():
 	st.title("PCA Image Reconstruction")
-
-
 
 STYLE = """
 <style>
@@ -71,7 +66,7 @@ def decomp(img_array, image_2d): # FUNCTION FOR RECONSTRUCTING 2D MATRIX USING P
 
     return p, cov_mat, eig_vec, loss
 
-def recons(no_of_comp, p, cov_mat, image_2d, eig_vec, in_image): 
+def recons(a_, no_of_comp, p, cov_mat, image_2d, eig_vec, in_image): 
         numpc = no_of_comp # THIS IS NUMBER OF PRINCIPAL COMPONENTS, YOU CAN CHANGE IT AND SEE RESULTS
         if numpc <p or numpc >0:
             eig_vec = eig_vec[:, range(numpc)]
@@ -80,12 +75,17 @@ def recons(no_of_comp, p, cov_mat, image_2d, eig_vec, in_image):
         recon = eigVec_score + np.mean(image_2d, axis = 1).T.reshape((eigVec_score.shape[0], 1)) # SOME NORMALIZATION CAN BE USED TO MAKE IMAGE QUALITY BETTER
         recon_img_mat = np.uint8(np.absolute(recon)) # TO CONTROL COMPLEX EIGENVALUES
 
-        return recon_img_mat, in_image
+        bits_per_pixel = a_.dtype.itemsize * a_.shape[-1] * 8
+        orig_size = (in_image.size[0]) * (in_image.size[1])#a_ * bits_per_pixel
+        print(f"Image size:{in_image.size}")
+        reduced_size = (no_of_comp)*(min(in_image.size[0], in_image.size[1])*2) #recon_img_mat * np.log2(no_of_comp+1)
+
+        return recon_img_mat, in_image, orig_size, reduced_size
 
 # """run this function"""
 # st.info(__doc__)
 st.markdown(STYLE, unsafe_allow_html=True)
-file = st.file_uploader("Upload an image", type=["png", "jpg"])
+file = st.file_uploader("Upload an image with lower size (preferably less than 50 KB)", type=["png", "jpg"])
 cont = st.empty()
 if not file:
 	# cont.info(f"Please Upload an image :{' '.join(['png', 'jpg'])}")
@@ -116,9 +116,9 @@ with st.spinner('LOADING'):
     no_of_comp = slider_comp
 
     # RECONSTRUCTING R,G,B COMPONENTS SEPARATELY
-    a_r_recon, in_image = recons(no_of_comp, p_r, cov_mat_r, a_r, eig_vec_r, in_image)
-    a_g_recon, in_image = recons(no_of_comp, p_g, cov_mat_g, a_g, eig_vec_g, in_image)
-    a_b_recon, in_image = recons(no_of_comp, p_b, cov_mat_b, a_b, eig_vec_b, in_image)
+    a_r_recon, in_image, orig_size_r, reduced_size_r = recons(a_r, no_of_comp, p_r, cov_mat_r, a_r, eig_vec_r, in_image)
+    a_g_recon, in_image, orig_size_g, reduced_size_g = recons(a_g, no_of_comp, p_g, cov_mat_g, a_g, eig_vec_g, in_image)
+    a_b_recon, in_image,  orig_size_b, reduced_size_b = recons(a_b, no_of_comp, p_b, cov_mat_b, a_b, eig_vec_b, in_image)
 
     recon_color_img = np.dstack((a_r_recon, a_g_recon, a_b_recon)) # COMBINING R.G,B COMPONENTS TO PRODUCE COLOR IMAGE
     recon_color_img = Image.fromarray(recon_color_img)
@@ -138,23 +138,11 @@ with st.spinner('LOADING'):
 
     img_buf = BytesIO()
     plt.savefig(img_buf, format='png')
-
     im = Image.open(img_buf)
     im = im.resize((450, 450))
     with st.container():
         st.write(f'Total Principal Components : {min(img_array.shape[0], img_array.shape[1])}')
-        # col1, col2 = st.columns(2)
-        # col1 = cont
-        caption_li=['Original Image','All three channels Reconstruction Loss' ,f'Resultant Image with {no_of_comp} components']
+        st.write(f'Compression Ratio: {100*(reduced_size_r+reduced_size_g+reduced_size_b)/(orig_size_r+orig_size_g+orig_size_b)}')
+        caption_li=['Original Image','All three channels Reconstruction Loss' ,f'Reconstructed Image with {no_of_comp} components']
         images = [new_image, im ,recon_color_img]
         st.image(images, caption=caption_li, width=400)
-        # with col1:
-        #     col1.image(new_image, caption='Original Image')
-        # with col2:
-        #     col2.image(recon_color_img, caption="Resultant Image")
-
-
-
-
-	
-	
